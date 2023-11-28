8660148eace18bb557f0387bb748c130f21140ea
NemiMoser <135074383+NemiMoser@users.noreply.github.com>
2023-11-28 13:35:45 -0500

    Update Product.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,14 +9,49 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    product_price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      defaultValue: 15,
+      validate: {
+        isDecimal: true
+      }
+    },
+    product_stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'category_id',
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: 'Product',
   }
 );
 
