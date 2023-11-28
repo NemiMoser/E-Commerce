75b4821a118c84a757ee93c50d8ef2d543192b6f
NemiMoser <135074383+NemiMoser@users.noreply.github.com>
2023-11-28 13:36:41 -0500

    Update schema.sql

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,13 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+
+CREATE TABLE IF NOT EXISTS `Product` (
+  `product_id` INTEGER auto_increment,
+  `product_name` VARCHAR(255) NOT NULL,
+  `product_price` DECIMAL(10,2) NOT NULL,
+  `product_stock` INTEGER NOT NULL DEFAULT 10,
+  `category_id` INTEGER,
+  PRIMARY KEY (`product_id`),
+  FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
